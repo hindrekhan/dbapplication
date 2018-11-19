ca9c725b14a224506b07c4145df9a86833113448
hindrekhan <hindrek.hannus@tptlive.ee>
2018-11-19 09:32:01 +0200

    abc

---
+++ dbapplication/dbapplication/MainActivity.cs
@@ -0,0 +1,27 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using SQLite;
+using System.IO;
+
+namespace dbapplication
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            string dbPath = Path.Combine(
+                System.Environment.GetFolderPath(
+                    System.Environment.SpecialFolder.Personal), "mydatabase.db3");
+            var db = new SQLiteConnection(dbPath);
+
+        }
+    }
+}
